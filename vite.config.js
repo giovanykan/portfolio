ff868edc777f3d505ff3071839aa9ee6af57dd34
giovanykan <giovanykantoro@gmail.com>
2026-07-13 15:49:49 +0700

    Fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: process.env.VERCEL ? '/' : '/portfolio/',
   plugins: [react()],
 });
